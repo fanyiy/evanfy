895022f8172c5e0a193ae0c8fce9cba05416f24b
Evan Yang <evanyang1026@gmail.com>
2023-04-28 16:32:10 +0200

    add metadata

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,30 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+
 export default function Document() {
+  const meta = {
+    title: "Evan's Portfolio",
+    description: "Evan's Portfolio",
+    image: '👋',
+    robots: 'follow, index',
+    twitter: '@__evanyang__',
+  }
+
   return (
     <Html lang="en" className='dark:bg-neutral-900 bg-neutral-100'>
-      <Head />
+      <Head>
+      <meta name="robots" content="follow, index" />
+        <meta name="description" content={meta.description} />
+        <meta property="og:site_name" content={meta.title} />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.image} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content={meta.twitter} />
+        <meta name="twitter:title" content={meta.title} />
+        <meta name="twitter:description" content={meta.description} />
+        <meta name="twitter:image" content={meta.image} />
+      </Head>
       <body>
         <Main />
         <NextScript />
