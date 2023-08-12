7c7a7334a7d4196acb1f14071a28cf3f393c8c79
Evan Yang <evanyang1026@gmail.com>
2023-08-12 16:58:10 +0800

    add keywords

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,16 +4,19 @@ export default function Document() {
   const meta = {
     title: "Evan Yang",
     description: "Evan Yang's personal website.",
+    keywords: ["personal website", "portfolio", "Evan Yang", "web development", "projects", "creative work"],
     image: "👋",
     robots: "follow, index",
     twitter: "@__evanyang__",
   };
+
 
   return (
     <Html lang="en" className="dark:bg-neutral-900 bg-neutral-100">
       <Head>
         <meta name="robots" content="follow, index" />
         <meta name="description" content={meta.description} />
+        <meta name="keywords" content={meta.keywords.join(", ")} />
         <meta property="og:site_name" content={meta.title} />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
