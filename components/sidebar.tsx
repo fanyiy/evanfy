b77f6655f9711b10357ed7ea653c5fd95c489916
Evan Yang <evanyang1026@gmail.com>
2023-08-17 15:00:10 +0800

    fix dark mode

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -26,8 +26,8 @@ export default function NavBar() {
   const [hoveredPath, setHoveredPath] = useState(pathname);
 
   return (
-    <div className="w-[130px] flex-shrink-0 border border-gray-200 bg-gray-200 rounded-lg">
-      <nav className="flex gap-0 relative justify-around z-[100] rounded-lg py-1">
+    <div className="w-[130px] flex-shrink-0 rounded-lg bg-gray-200 dark:bg-neutral-800">
+      <nav className="flex gap-0 relative justify-around z-[100] rounded-lg py-1.5">
         {navItems.map((item) => {
           const isActive = item.path === pathname;
 
@@ -35,7 +35,9 @@ export default function NavBar() {
             <Link
               key={item.path}
               className={`px-2.5 py-1.5 rounded-md text-sm lg:text-md font-medium relative no-underline duration-300 ease-in ${
-                isActive ? "text-gray-800" : "text-gray-500"
+                isActive
+                  ? "text-gray-800 dark:text-neutral-200"
+                  : "text-gray-500 dark:text-neutral-400"
               }`}
               data-active={isActive}
               href={item.path}
@@ -44,19 +46,12 @@ export default function NavBar() {
               <span>{item.name}</span>
               {item.path === hoveredPath && (
                 <motion.div
-                  className="absolute bottom-0 left-0 h-full bg-white rounded-md -z-10"
+                  className="absolute bottom-0 left-0 h-full bg-white dark:bg-neutral-700 rounded-md -z-10"
                   layoutId="navbar"
                   aria-hidden="true"
                   style={{
                     width: "100%",
                   }}
-                  //   transition={{
-                  //     type: "spring",
-                  //     bounce: 0.25,
-                  //     stiffness: 130,
-                  //     damping: 9,
-                  //     duration: 0.3,
-                  //   }}
                 />
               )}
             </Link>
