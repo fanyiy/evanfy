603f3aeb45992e99176b2352f523cc068164553e
Evan Yang <evanyang1026@gmail.com>
2023-09-19 10:46:01 +0200

    sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,17 @@
+import { allBlogs } from 'contentlayer/generated';
+
+export default async function sitemap() {
+  const blogs = allBlogs.map((post) => ({
+    url: `https://evanyang.dev/blog/${post.slug}`,
+    lastModified: post.publishedAt,
+  }));
+
+  const routes = ['', '/blog'].map(
+    (route) => ({
+      url: `https://evanyang.dev${route}`,
+      lastModified: new Date().toISOString().split('T')[0],
+    })
+  );
+
+  return [...routes, ...blogs];
+}
