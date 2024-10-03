56ba6f8f074bd5284e0ce6e18a1564649cbdafac
Evan Yang <evanyang1026@gmail.com>
2024-10-03 09:44:19 +0200

    Edge runtime

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -6,6 +6,8 @@ import { getTweets } from "lib/twitter";
 import PostTitle from "components/post-title";
 import { ArrowUpRight } from "lucide-react";
 
+export const runtime = 'edge';
+
 export async function generateMetadata({
   params,
 }): Promise<Metadata | undefined> {
@@ -107,21 +109,21 @@ export default async function Blog({ params }) {
       <Mdx code={post.body.code} />
       <ul className="antialiased flex flex-col mt-8 gap-2 text-neutral-600 dark:text-neutral-300">
         <a
-          href="https://twitter.com/__evanyang__"
+          href="https://twitter.com/TheEvanYang"
           target="_blank"
           className="flex items-center hover:text-neutral-800 dark:hover:text-neutral-100 transition-all"
         >
           <ArrowUpRight className="w-5 h-5 mr-2" />
           Follow me
         </a>
-        <a
+        {/* <a
           href="https://evanyang.substack.com/"
           target="_blank"
           className="flex items-center hover:text-neutral-800 dark:hover:text-neutral-100 transition-all"
         >
           <ArrowUpRight className="w-5 h-5 mr-2" />
           Get email updates
-        </a>
+        </a> */}
       </ul>
     </section>
   );
