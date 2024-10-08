cd85424a577185a1e0949bbd34950c0ca67f8b5a
Evan Yang <evanyang1026@gmail.com>
2024-10-08 15:58:04 +0200

    Remove blog for now

--- app/layout.tsx
+++ app/layout.tsx
@@ -73,11 +73,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${noto_sans.className} max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto dark:bg-neutral-900 dark:text-white bg-neutral-50`}
+        className={`${noto_sans.className} max-w-2xl py-24 flex flex-col md:flex-row mx-4 lg:mx-auto dark:bg-neutral-900 dark:text-white bg-neutral-50`}
         suppressHydrationWarning={true}
       >
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
-          <Navbar />
+          {/* <Navbar /> */}
           <Providers attribute="class" defaultTheme="system" enableSystem>
             {children}
           </Providers>
