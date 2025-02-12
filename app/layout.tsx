94262f1a6904643c1ac75de2c77eeb9d2abc6cb2
Evan Yang <evanyang1026@gmail.com>
2025-02-12 14:15:57 +0800

    Add Clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -79,6 +79,19 @@ export default function RootLayout({
           data-domain="evanfy.com"
           src="https://plausible.evanfy.com/js/script.js"
         />
+        <Script
+          id="ms_clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", 'q8jgqumg2j');`,
+          }}
+        />
+
       </body>
     </html>
   );
