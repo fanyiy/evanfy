2c24a3aca6562ddbfb0191a53e2fd477f9344087
Evan Yang <evanyang1026@gmail.com>
2025-02-13 22:29:34 +0800

    FIx

--- actions/check-domain-availability.ts
+++ actions/check-domain-availability.ts
@@ -1,3 +1,43 @@
+// "use server";
+
+// interface DomainCheck {
+//   names: string[];
+//   tlds: string[];
+// }
+
+// export async function checkDomains(domains: string[]) {
+//   // Split domain names into name and TLD arrays
+//   const names: string[] = [];
+//   const tlds = new Set<string>();
+
+//   domains.forEach((domain) => {
+//     const lastDotIndex = domain.lastIndexOf(".");
+//     names.push(domain);
+//     tlds.add(domain.slice(lastDotIndex + 1));
+//   });
+
+//   const res = await fetch(
+//     "https://n8n.evanfy.com/webhook/check-domain-availability",
+//     {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({
+//         names,
+//         tlds: Array.from(tlds),
+//       }),
+//     }
+//   );
+
+//   const data = await res.json();
+
+//   console.log(data);
+
+//   return data.domains;
+// }
+
+
 "use server";
 
 export async function checkDomains(domains: string[]) {
